40a0203fe72454fbd25a2e71c0c518815c9dcd6c
1000000000 <lthyde@gmail.com>
2016-09-14 01:28:56 -0400

    Improved command line flags parsing added new flag
    Added -e flag to use code from its argument instead of the
    source file. Reordered the arguments into alphabetical order and
    switched to using built in error messages for missing flag argument
    and unrecognized flag.

--- main.c
+++ main.c
@@ -4,8 +4,6 @@
 
 void print_help_string(char* prog) {
 	fprintf(stderr, "Usage: %s [switches] [--] [programfile] [arguments]\n"
-	                "  -f file     Read arguments for Brain-Flak program from the specified file,\n"
-	                "                ignore arguments from command line.\n"
 	                "  -a          Convert arguments to their ASCII codepoint values and output in\n"
 	                "                decimal. This overrides previous -A and -c flags.\n"
 	                "  -A          Treat arguments as decimal numbers and convert output to ASCII\n"
@@ -14,6 +12,9 @@ void print_help_string(char* prog) {
 					"  -c          Convert arguments to their ASCII codepoint values and convert\n"
 	                "                output to ASCII text, modding by 256 before doing so. This\n"
 	                "                overrides previous -a and -A flags.\n"
+	                "  -e program  Runs the specified program instead of reading from the source file.\n"
+	                "  -f file     Read arguments for Brain-Flak program from the specified file,\n"
+	                "                ignore arguments from command line.\n"
 	                "  -h          Display this information.\n"
 	                "  -v          Display interpreter version info.\n",
 	prog);
@@ -21,12 +22,9 @@ void print_help_string(char* prog) {
 
 int main(int argc, char* argv[]) {
 	uint8_t ascii_in = 0, ascii_out = 0;
-	char c, *arg_file = NULL;
-	while ((c = getopt(argc, argv, "+:f:aAchv")) != -1) {
+	char c, *arg_file = NULL, *program = NULL;
+	while ((c = getopt(argc, argv, "+aAcef:hv")) != -1) {
 		switch (c) {
-			case 'f':
-				arg_file = optarg;
-				break;
 			case 'a':
 				ascii_in  = 1;
 				ascii_out = 0;
@@ -39,18 +37,19 @@ int main(int argc, char* argv[]) {
 				ascii_in  = 1;
 				ascii_out = 1;
 				break;
+			case 'e':
+				program = optarg;
+				break;
+			case 'f':
+				arg_file = optarg;
+				break;
 			case 'h':
 				print_help_string(argv[0]);
 				return 0;
 			case 'v':
 				fprintf(stderr, "CraneFlak version 1.0.0 DEV\n");
 				return 0;
-			case ':':
-				fprintf(stderr, "%s: option requires an argument -- '%c'\n", argv[0], optopt);
 			case '?':
-				if (c == '?') { // Make sure this isn't from falling through
-					fprintf(stderr, "%s: invalid option -- '%c'\n", argv[0], optopt);
-				}
 				fprintf(stderr, "Try '%s -h' for more information.\n", argv[0]);
 				return 1;
 		}
