b7855f88e1f08d3af18217dfacd49de51807c53a
1000000000 <lthyde@gmail.com>
2016-09-19 14:16:59 -0400

    Wrote most of interpreter_run
    Stll lacks error handling and the ability to deal
    with nonseekable streams

--- interpreter.c
+++ interpreter.c
@@ -61,8 +61,143 @@ interpreter* interpreter_new(FILE* source, data_stack* on, data_stack* off) {
 	return interp;
 }
 
+int is_open_brace(char c) {
+	return c == '(' || c == '[' || c == '{' || c == '<';
+}
+
+int is_close_brace(char c) {
+	return c == ')' || c == ']' || c == '}' || c == '>';
+}
+
+int is_matching_brace(char b1, char b2) {
+	return (b1 == '(' && b2 == ')') || (b1 == '[' && b2 == ']') || (b1 == '{' && b2 == '}') || (b1 == '<' && b2 == '>');
+}
+
+int skip_loop(FILE* source) {
+	char c;
+	int paren_scope = 0, squar_scope = 0, curly_scope = 1, angle_scope = 0;
+	while (curly_scope != 0) {
+		if (feof(source)) {
+			return 1;
+		}
+		c = fgetc(source);
+		switch (c) {
+			case '(':
+				++paren_scope;
+				break;
+			case ')':
+				--paren_scope;
+				break;
+			case '[':
+				++squar_scope;
+				break;
+			case ']':
+				--squar_scope;
+				break;
+			case '{':
+				++curly_scope;
+				break;
+			case '}':
+				--curly_scope;
+				break;
+			case '<':
+				++angle_scope;
+				break;
+			case '>':
+				--angle_scope;
+				break;
+		}
+		if (paren_scope < 0 || squar_scope < 0 || angle_scope < 0) return 1;
+	}
+	if (paren_scope || squar_scope || angle_scope) return 1;
+	return 0;
+}
+
 int interpreter_run(interpreter* interp) {
-	return 0; // TODO write more here
+	char next, last = 0;
+	while(!feof(interp->source)) {
+		next = fgetc(interp->source);
+		if (is_matching_brace(last, next)) { // nilads
+			switch (last) {
+				case '(':
+					++interp->current_value;
+					break;
+				case '[':
+					interp->current_value += data_stack_height(*interp->active_stack);
+					break;
+				case '{':
+					interp->current_value += data_stack_peek(*interp->active_stack);
+					*interp->active_stack = data_stack_pop(*interp->active_stack);
+					break;
+				case '<':
+					if (interp->active_stack == interp->data) {
+						interp->active_stack = interp->data + 1;
+					} else {
+						interp->active_stack = interp->data;
+					}
+					break;
+				default:
+					fprintf(stderr, "Error\n");
+					// should never happen but erroring here helps with debugging
+			}
+			last = 0;
+		} else if (is_close_brace(next)) {
+			if (last) {
+				// error mismatched braces
+			}
+			if (!interp->scope) {
+				// error unmatched closing brace
+			}
+			if (!is_matching_brace(interp->scope->symbol, next)) {
+				// error mismatched braces
+			}
+			switch(next) {
+				case ')':
+					*interp->active_stack = data_stack_push(*interp->active_stack, interp->current_value);
+					break;
+				case ']':
+					interp->current_value *= -1;
+					break;
+				case '>':
+					interp->current_value = 0;
+					break;
+				case '}':
+					if (data_stack_peek(*interp->active_stack)) {
+						fseek(interp->source, interp->scope->index, SEEK_SET);
+						interp->scope->current_value += interp->current_value;
+						interp->current_value = 0;
+					}
+					break;
+				default:
+					fprintf(stderr, "Error\n");
+					// should never happen but erroring here may help with debugging
+			}
+			if (next != '}' || !data_stack_peek(*interp->active_stack)) {
+				interp->current_value += interp->scope->current_value;
+				interp->scope = scope_stack_pop(interp->scope);
+			}
+		} else if (is_open_brace(next)) {
+			if (last == '{') {
+				if (data_stack_peek(*interp->active_stack)) {
+					scope_stack_push(interp->scope, interp->current_value, last, ftell(interp->source)); // index needs to be fixed
+					interp->current_value = 0;
+				} else {
+					if (!skip_loop(interp->source)) {
+						// error
+					}
+					next = 0;
+				}
+			} else if (last) {
+				scope_stack_push(interp->scope, interp->current_value, last, ftell(interp->source)); // index needs to be fixed
+				interp->current_value = 0;
+			}
+			last = next;
+		}
+	}
+	if (last || interp->scope) {
+		// error
+	}
+	return 0;
 }
 
 data_stack* interpreter_remove_active_stack(interpreter* interp) {
