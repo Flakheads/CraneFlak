d9ede110f526d77ee1d82a3db64d253eba6d838a
1000000000 <lthyde@gmail.com>
2016-10-05 15:56:22 -0400

    Finished interpreter, sans error handling
    Interpreter no longer distinguishes between seekable and nonseekable streams
    The buffer is used for both

--- interpreter.c
+++ interpreter.c
@@ -1,6 +1,5 @@
 #include "interpreter.h"
 
-#include <errno.h>
 #include <inttypes.h>
 #include <stdlib.h>
 
@@ -35,7 +34,7 @@ void scope_stack_free(scope_stack* stack) {
 typedef struct interpreter {
 	FILE* source;
 	char* buf;
-	long buf_size, buf_len, buf_index;
+	long index, buf_offset, buf_size, buf_len;
 	scope_stack* scope;
 	data_stack* data[2];
 	data_stack** active_stack;
@@ -44,20 +43,14 @@ typedef struct interpreter {
 } interpreter;
 
 interpreter* interpreter_new(FILE* source, data_stack* on, data_stack* off) {
-	int errno_tmp = errno;
 	interpreter* interp = (interpreter*) malloc(sizeof(interpreter));
 	interp->source = source;
-	errno = 0;
-	// if source is not seekable then set up a buffer
-	if (fseek(source, 0L, SEEK_CUR) == -1 && errno == EBADF) {
-		interp->buf_size = 0;
-		interp->buf_len = 0;
-		interp->buf_index = 0;
-		interp->buf = NULL;
-	} else {
-		interp->buf_size = -1;
-	}
-	errno = errno_tmp;
+	interp->buf = NULL;
+	interp->index = 0;
+	interp->buf_offset = 0;
+	interp->buf_size = 0;
+	interp->buf_len = -1;
+	interp->scope = NULL;
 	interp->data[0] = on;
 	interp->data[1] = off;
 	interp->active_stack = interp->data;
@@ -77,14 +70,43 @@ int is_matching_brace(char b1, char b2) {
 	return (b1 == '(' && b2 == ')') || (b1 == '[' && b2 == ']') || (b1 == '{' && b2 == '}') || (b1 == '<' && b2 == '>');
 }
 
-int skip_loop(FILE* source) {
-	char c;
-	int paren_scope = 0, squar_scope = 0, curly_scope = 1, angle_scope = 0;
-	while (curly_scope != 0) {
-		if (feof(source)) {
-			return 1;
+void interpreter_buf_push(interpreter* interp, int c) {
+	if (interp->buf_len == -1) return;
+	if (interp->buf_len >= interp->buf_size) {
+		if (interp->buf_size == 0) {
+			interp->buf_size = 4;
+		} else {
+			interp->buf_size *= 2;
 		}
-		c = fgetc(source);
+		interp->buf = (char*) realloc(interp->buf, sizeof(char)*interp->buf_size);
+	}
+	interp->buf[interp->buf_len] = c;
+	++interp->buf_len;
+}
+
+int interpreter_getc(interpreter* interp) {
+	int c;
+	if (interp->index >= interp->buf_offset + interp->buf_len) {
+		c = fgetc(interp->source);
+		if (c != EOF) {
+			interpreter_buf_push(interp, c);
+		}
+	} else {
+		c = interp->buf[interp->index - interp->buf_offset];
+	}
+	return c;
+}
+
+int interpreter_skip_loop(interpreter* interp, char first) {
+	int c = first, paren_scope = 0, squar_scope = 0, curly_scope = 1, angle_scope = 0, first_iter = 1;
+	while (curly_scope != 0) {
+		if (!first_iter) {
+			if (feof(interp->source)) {
+				return 1;
+			}
+			c = interpreter_getc(interp);
+		}
+		first_iter = 0;
 		switch (c) {
 			case '(':
 				++paren_scope;
@@ -110,7 +132,10 @@ int skip_loop(FILE* source) {
 			case '>':
 				--angle_scope;
 				break;
+			case EOF:
+				return 1;
 		}
+		++interp->index;
 		if (paren_scope < 0 || squar_scope < 0 || angle_scope < 0) return 1;
 	}
 	if (paren_scope || squar_scope || angle_scope) return 1;
@@ -118,9 +143,11 @@ int skip_loop(FILE* source) {
 }
 
 int interpreter_run(interpreter* interp) {
+	unsigned curly_depth = 0;
+	long last_index = -1;
 	char next, last = 0;
 	while(!feof(interp->source)) {
-		next = fgetc(interp->source);
+		next = interpreter_getc(interp);
 		if (is_matching_brace(last, next)) { // nilads
 			switch (last) {
 				case '(':
@@ -132,6 +159,10 @@ int interpreter_run(interpreter* interp) {
 				case '{':
 					interp->current_value += data_stack_peek(*interp->active_stack);
 					*interp->active_stack = data_stack_pop(*interp->active_stack);
+					--curly_depth;
+					if (curly_depth == 0) {
+						interp->buf_len = -1;
+					}
 					break;
 				case '<':
 					if (interp->active_stack == interp->data) {
@@ -141,18 +172,21 @@ int interpreter_run(interpreter* interp) {
 					}
 					break;
 				default:
-					fprintf(stderr, "Error\n");
+					fprintf(stderr, "Error0\n");
 					// should never happen but erroring here helps with debugging
 			}
 			last = 0;
 		} else if (is_close_brace(next)) {
 			if (last) {
+				fprintf(stderr, "Error1\n");
 				// error mismatched braces
 			}
 			if (!interp->scope) {
+				fprintf(stderr, "Error2\n");
 				// error unmatched closing brace
 			}
 			if (!is_matching_brace(interp->scope->symbol, next)) {
+				fprintf(stderr, "Error3\n");
 				// error mismatched braces
 			}
 			switch(next) {
@@ -167,13 +201,18 @@ int interpreter_run(interpreter* interp) {
 					break;
 				case '}':
 					if (data_stack_peek(*interp->active_stack)) {
-						fseek(interp->source, interp->scope->index, SEEK_SET);
+						interp->index = interp->scope->index;
 						interp->scope->current_value += interp->current_value;
 						interp->current_value = 0;
+					} else {
+						--curly_depth;
+						if (curly_depth == 0) {
+							interp->buf_len = -1;
+						}
 					}
 					break;
 				default:
-					fprintf(stderr, "Error\n");
+					fprintf(stderr, "Error4\n");
 					// should never happen but erroring here may help with debugging
 			}
 			if (next != '}' || !data_stack_peek(*interp->active_stack)) {
@@ -181,24 +220,31 @@ int interpreter_run(interpreter* interp) {
 				interp->scope = scope_stack_pop(interp->scope);
 			}
 		} else if (is_open_brace(next)) {
-			if (last == '{') {
-				if (data_stack_peek(*interp->active_stack)) {
-					scope_stack_push(interp->scope, interp->current_value, last, ftell(interp->source)); // index needs to be fixed
-					interp->current_value = 0;
-				} else {
-					if (!skip_loop(interp->source)) {
-						// error
+			// this tells the program to start storing code in buffer
+			if (next == '{') {
+					if (interp->buf_len == -1) {
+						interp->buf_offset = interp->index + 1;
+						interp->buf_len = 0;
 					}
-					next = 0;
+					++curly_depth;
+			}
+			if (last == '{' && !data_stack_peek(*interp->active_stack)) {
+				if (interpreter_skip_loop(interp, next) != 0) {
+					fprintf(stderr, "Error5\n");
+					// error
 				}
+				next = 0;
 			} else if (last) {
-				scope_stack_push(interp->scope, interp->current_value, last, ftell(interp->source)); // index needs to be fixed
+				interp->scope = scope_stack_push(interp->scope, interp->current_value, last, last_index);
 				interp->current_value = 0;
 			}
 			last = next;
+			last_index = interp->index;
 		}
+		++interp->index;
 	}
 	if (last || interp->scope) {
+		fprintf(stderr, "Error6\n");
 		// error
 	}
 	return 0;
